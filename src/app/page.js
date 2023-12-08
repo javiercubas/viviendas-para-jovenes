f8ae9327fda0732bee80bd8a9c87c2bd414a66d2
javiercubas <66169702+javiercubas@users.noreply.github.com>
2023-12-08 17:05:36 +0100

    update

--- src/app/page.js
+++ src/app/page.js
@@ -15,12 +15,6 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <section className={styles.hero}>
-        <div className={styles.header}>
-          <a href='/' className={styles.title}>VIVIENDAS PARA JÓVENES</a>
-        </div>
-        <Image src="/logotipo-mitmo-miviau.svg" width={334} height={74} />
-        <Image src="/image2.png" width={1920} height={200} />
-        <Image src="/image3.png" width={210} height={240} />
         <div className={styles.description}>
           <h2 className={styles.subtitle}>PROGRAMAS DE AYUDA Y SUBVENCIÓN A JÓVENES</h2>
           <div style={{ display: 'flex', justifyContent: 'center' }}>
@@ -29,23 +23,35 @@ export default function Home() {
 
               <h2 className={styles.subtitle}>COMPRA</h2>
 
-              <p className={styles.text}>CONSIGUE TU AYUDA DE HASTA 10.800 EUROS POR LA COMPRA DE TU PRIMERA VIVIENDA.<br></br> <br></br> CONSIGUE EL 95% DE FINANCIACIÓN EN TU HIPOTECA</p>
+              <p className={styles.text}>CONSIGUE TU AYUDA DE HASTA 10.800 EUROS POR LA COMPRA DE TU PRIMERA VIVIENDA.<br></br> <br></br> CONSIGUE EL 95% DE FINANCIACIÓN EN TU HIPOTECA.</p>
 
-              <form className={styles.form}>
-                <input type="email" placeholder="Introduce tu email" />
-                <button type="submit">Estoy interesado</button>
-              </form>
+              <div style={{ display: 'flex', backgroundColor: 'white', border: '5px solid black', padding: '20px', borderRadius: '10px' }}>
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'space-between' }}>
+                  <Image src="/logotipo-mitmo-miviau.svg" width={335} height={74} />
+                  <Image src="/image2.png" width={335} height={74} />
+                </div>
+                <Image src="/image3.png" width={130} height={148} />
+              </div>
             </div>
             <div className={styles.right}>
               <h2 className={styles.subtitle}>ALQUILER</h2>
 
-              <p className={styles.text}>CONSIGUE TU BONO DE AYUDA DE 6.000 EUROS.<br></br> <br></br> REBAJA HASTA EL 75% LA RENTA</p>
+              <p className={styles.text}>CONSIGUE TU BONO DE AYUDA DE 6.000 EUROS.<br></br> <br></br> <br></br> REBAJA HASTA EL 75% LA RENTA.</p>
+
+              <Image src='/alquiler.jpeg' width={395} height={208} />
             </div>
           </div>
+          <form className={styles.form}>
+            <input type="email" placeholder="Introduce tu email" />
+            <input type="text" placeholder="Introduce tu nombre" />
+            <input type="tel" placeholder="Introduce tu teléfono" />
+            <button type="submit">Estoy interesado</button>
+          </form>
         </div>
       </section>
       <div className={styles.footer}>
         <Image src="/footer.jpg" width={1920} height={160} />
+        <p>Todas las imágenes y logos son propiedad de sus factores. Son utilizados en la web para una mejora en la identificación de los servicios privados a prestar por VIVIENDASPARAJÓVENES + AYUDAYSUBVENCIÓN por la consultora de abogados y economistas Winner Markets S.L. con CIF B84148741. <br></br> © 2023-2024 VIVIENDASPARAJOVENES.COM</p>
       </div>
     </main >
   )
