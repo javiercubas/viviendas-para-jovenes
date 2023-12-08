6cd39cc3602031493e0ee303af53512956fe9aa3
javiercubas <66169702+javiercubas@users.noreply.github.com>
2023-12-08 16:20:00 +0100

    update

--- src/app/page.js
+++ src/app/page.js
@@ -22,22 +22,25 @@ export default function Home() {
         <Image src="/image2.png" width={1920} height={200} />
         <Image src="/image3.png" width={210} height={240} />
         <div className={styles.description}>
-          <div className={styles.left}>
-            <h2 className={styles.subtitle}>PROGRAMAS DE AYUDA Y SUBVENCIÓN A JÓVENES</h2>
+          <h2 className={styles.subtitle}>PROGRAMAS DE AYUDA Y SUBVENCIÓN A JÓVENES</h2>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <div className={styles.left}>
 
-            <h2 className={styles.subtitle}>COMPRA</h2>
 
-            <p className={styles.text}>CONSIGUE TU AYUDA DE HASTA 10.800 EUROS POR LA COMPRA DE TU PRIMERA VIVIENDA.<br></br> <br></br> CONSIGUE EL 95% DE FINANCIACIÓN EN TU HIPOTECA</p>
+              <h2 className={styles.subtitle}>COMPRA</h2>
 
-            <form className={styles.form}>
-              <input type="email" placeholder="Introduce tu email" />
-              <button type="submit">Estoy interesado</button>
-            </form>
-          </div>
-          <div className={styles.right}>
-            <h2 className={styles.subtitle}>ALQUILER</h2>
+              <p className={styles.text}>CONSIGUE TU AYUDA DE HASTA 10.800 EUROS POR LA COMPRA DE TU PRIMERA VIVIENDA.<br></br> <br></br> CONSIGUE EL 95% DE FINANCIACIÓN EN TU HIPOTECA</p>
 
-            <p className={styles.text}>CONSIGUE TU BONO DE AYUDA DE 6.000 EUROS.<br></br> <br></br> REBAJA HASTA EL 75% LA RENTA</p>
+              <form className={styles.form}>
+                <input type="email" placeholder="Introduce tu email" />
+                <button type="submit">Estoy interesado</button>
+              </form>
+            </div>
+            <div className={styles.right}>
+              <h2 className={styles.subtitle}>ALQUILER</h2>
+
+              <p className={styles.text}>CONSIGUE TU BONO DE AYUDA DE 6.000 EUROS.<br></br> <br></br> REBAJA HASTA EL 75% LA RENTA</p>
+            </div>
           </div>
         </div>
       </section>
